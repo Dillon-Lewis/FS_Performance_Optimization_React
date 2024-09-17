80a058680d79c46ff7be6ac2c37cb81388159b71
Dillon-Lewis <dilllewis42@gmail.com>
2024-09-17 13:49:06 -0400

    first Commit

---
+++ src/components/PostContent.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { memo } from "react";
+
+const PostContent = memo(function PostContent({ content }) {
+  
+  return (
+    <div>
+      {content ? (
+        <>
+          
+          <h2>{content.title}</h2>
+          <p>{content.body}</p>
+        </>
+      ) : (
+        <>
+          <h2>Select a post</h2>
+        </>
+      )}
+    </div>
+  );
+});
+
+export default PostContent;
